4bf535c549576376b105dc36cd4aa06eaf478310
rubelmah55 <rubelmah55@gmail.com>
2018-01-24 12:29:55 +0600

    change log in page

--- functions.php
+++ functions.php
@@ -103,3 +103,45 @@ function vc_before_init_actions() {
      
 }
 
+/* customize login screen */
+function charity_images_custom_login_page() {
+    echo '<style type="text/css">
+        .login h1 a { background-image:url("'. get_stylesheet_directory_uri().'/images/logo.png") !important; height: 100px !important; width: 100% !important; margin: 0 auto !important; background-size: contain !important; }
+		h1 a:focus { outline: 0 !important; box-shadow: none; }
+        body.login { background-image:url("'. get_stylesheet_directory_uri().'/images/banner.jpg") !important; background-repeat: no-repeat !important; background-attachment: fixed !important; background-position: center !important; background-size: cover !important; position: relative; z-index: 999;}
+  		body.login:before {background-color: #42b1e67a; position: absolute; width: 100%; height: 100%; left: 0; top: 0; content: ""; z-index: -1; }
+  		.login form {
+  			background: rgba(255,255,255, 0.2) !important;
+  		}
+		.login form .input, .login form input[type=checkbox], .login input[type=text] {
+			background: transparent !important;
+			color: #ddd;
+		}
+		.login label {
+			color: #DDD !important;
+		}
+		.login #login_error, .login .message {
+			color: #ddd;
+			margin-top: 20px;
+			background: rgba(255,255,255, 0.2) !important;
+		}
+		#login {
+		    padding: 7% 0 0;
+		}
+		
+		.login #nav a, .login #backtoblog a, .login label, .login .message{
+			color:#000 !important;
+		}
+    </style>';
+}
+add_action('login_head', 'charity_images_custom_login_page', 99);
+function cabinet_login_logo_url_title() {
+ 	return 'Business Simple';
+}
+add_filter( 'login_headertitle', 'cabinet_login_logo_url_title' );
+function cabinet_login_logo_url() {
+	return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'cabinet_login_logo_url' );
+
+
